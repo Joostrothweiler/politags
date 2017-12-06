2172f2b7d34498b6f05ab68c23a190dab46228c6
Max van Zoest <maxvanzoest@me.com>
2017-12-06 15:40:32 +0100

    model alpha

--- app/models/models.py
+++ app/models/models.py
@@ -2,6 +2,7 @@ from app import db
 import datetime
 from sqlalchemy.dialects import postgresql
 
+
 # This file defines all the different models we use.
 # Migrations are automatically generated based on these classes.
 # After changes, use python manage.py db revision --autogenerate -m "Present tense message"
@@ -13,7 +14,7 @@ class Article(db.Model):
 
     # Relationships
     entities = db.relationship('Entity', secondary='articles_entities',
-                        backref=db.backref('articles', lazy='dynamic'))
+                               backref=db.backref('articles', lazy='dynamic'))
 
 
 class Entity(db.Model):
@@ -37,23 +38,44 @@ class Party(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 
-# Define the ArticleEntities association model
-class ArticlesEntities(db.Model):
-    __tablename__ = 'articles_entities'
-    id = db.Column(db.Integer(), primary_key=True)
-    article_id = db.Column(db.String(200), db.ForeignKey('articles.id', ondelete='CASCADE'))
-    entity_id = db.Column(db.Integer(), db.ForeignKey('entities.id', ondelete='CASCADE'))
-
-
 class Question(db.Model):
     __tablename__ = 'questions'
     id = db.Column(db.Integer(), primary_key=True)
+    questionable_type = db.Column(db.String(20))
+    questionable_id = db.Column(db.Integer())
     possible_answers = db.Column(postgresql.ARRAY(db.String(20), dimensions=1))
     responses = db.relationship("Response")
 
 
 class Response(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    question_id = db.Column(db.Integer(), db.ForeignKey('questions.id'))
     response = db.Column(db.String(100))
     question = db.relationship("Question", back_populates="responses")
+
+
+# RELATIONS
+
+class EntitiesParties(db.Model):
+    __tablename__ = 'entities_parties'
+    id = db.Column(db.Integer, primary_key=True)
+    entity_id = db.Column(db.Integer(), db.ForeignKey('entities.id'))
+    party_id = db.Column(db.Integer(), db.ForeignKey('parties.id'))
+    certainty = db.Column(db.Float(), default=0.0)
+
+
+class EntitiesPoliticians(db.Model):
+    __tablename__ = 'entities_politicians'
+    id = db.Column(db.Integer, primary_key=True)
+    entity_id = db.Column(db.Integer(), db.ForeignKey('entities.id'))
+    politician_id = db.Column(db.Integer(), db.ForeignKey('politicians.id'))
+    certainty = db.Column(db.Float(), default=0.0)
+
+
+class ArticlesEntities(db.Model):
+    __tablename__ = 'articles_entities'
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.String(200), db.ForeignKey('articles.id'))
+    entity_id = db.Column(db.Integer(), db.ForeignKey('entities.id'))
+    start_pos = db.Column(db.Integer())
+    end_pos = db.Column(db.Integer())
