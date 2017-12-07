9a5feec66a00913d905b2d5bd9ea4efd6d7de6e3
joostrothweiler <joostrothweiler@gmail.com>
2017-12-07 15:05:37 +0100

    Some NER and NED implemented and working with seeds

--- app/modules/entities/extract.py
+++ app/modules/entities/extract.py
@@ -2,7 +2,7 @@ import nl_core_news_sm
 
 from spacy.matcher import PhraseMatcher
 from app import db
-from app.models.models import Article, Entity, Politician, Party, EntitiesPoliticians
+from app.models.models import Article, Entity, Politician, Party, EntitiesPoliticians, EntitiesParties
 from app.modules.common.utils import collection_as_dict, string_similarity
 
 nlp = nl_core_news_sm.load()
@@ -31,27 +31,27 @@ def process_new_document(document):
     # Then process the NER and save entities + disambiguation certainties
     doc = nlp(str(document['description']))
     for ent in doc.ents:
-        entity = Entity(text = ent.text,
-                        label = ent.label_,
-                        start_pos = ent.start_char,
-                        end_pos = ent.end_char)
-        new_article.entities.append(entity)
-        db.session.add(entity)
-        # Store disambiguation certainties
-        store_disambiguation(entity)
+        if len(ent.text) > 1 and len(ent.text) < 50:
+            entity = Entity(text = ent.text,
+                            label = ent.label_,
+                            start_pos = ent.start_char,
+                            end_pos = ent.end_char)
+            new_article.entities.append(entity)
+            db.session.add(entity)
+            # Store disambiguation certainties
+            store_disambiguation(entity)
 
     # Commit to db
     db.session.commit()
-
     # Return what we know.
     return get_existing_knowledge(new_article)
 
 
 def store_disambiguation(entity):
-    if(entity.label == 'PER'):
+    if entity.label == 'PER':
         store_politician_disambiguation(entity)
 
-    if(entity.label == 'ORG'):
+    if entity.label == 'ORG':
         store_party_disambiguation(entity)
 
 
@@ -60,7 +60,7 @@ def store_politician_disambiguation(entity):
 
     for politician in possible_politicians:
         sim = string_similarity(politician.full_name, entity.text)
-        if sim > 0.9:
+        if sim > 0.4:
             a = EntitiesPoliticians(certainty = sim)
             a.politician = politician
             entity.politicians.append(a)
@@ -72,8 +72,8 @@ def store_party_disambiguation(entity):
 
     for party in possible_parties:
         sim = string_similarity(party.name, entity.text)
-        if sim > 0.9:
-            a = EntitiesPoliticians(certainty = sim)
-            a.politician = politician
-            entity.politicians.append(a)
+        if sim > 0.7:
+            a = EntitiesParties(certainty = sim)
+            a.party = party
+            entity.parties.append(a)
             db.session.add(entity)
