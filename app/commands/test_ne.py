5663ab886c2e3c6629f0498c589471990f640126
Joost Rothweiler <joostrothweiler@gmail.com>
2017-12-20 15:21:12 +0100

    some work on testing ner

--- app/commands/test_ne.py
+++ app/commands/test_ne.py
@@ -2,6 +2,7 @@ import json
 
 from flask_script import Command
 
+from app import post_article_ner
 from app.models.models import Entity, Article, EntityLinking
 from app.modules.common.utils import translate_doc
 from app.modules.entities.named_entities import process_document
@@ -18,8 +19,14 @@ def test_ne():
     """ Test Named Entity Algorithms."""
     print('Deleting all old data')
     remove_all_articles()
-    print('Running sample articles with NER')
-    init_sample_articles()
+
+    input = 'data_resources/evaluation/van_dijk_input.json'
+    output = process_evaluation_input(input)
+
+    eval = 'data_resources/evaluation/van_dijk_eval.json'
+    eval_output = json.load(open(eval))
+
+    evaluate_ned(output, eval_output)
 
 
 def remove_all_articles():
@@ -31,9 +38,56 @@ def remove_all_articles():
     Article.query.delete()
 
 
-def init_sample_articles():
-    samples = json.load(open('data_resources/poliflow_sample_van_dijk.json'))
-    items = samples['item']
+def process_evaluation_input(input):
+    samples = json.load(open(input))
+    items = samples['items']
+
+    output = {'items': []}
 
     for doc in items:
-        process_document(translate_doc(doc))
+        simple_doc = translate_doc(doc)
+        res = process_document(simple_doc)
+        output['items'].append(res)
+
+    return output
+
+def evaluate_ned(output, eval_output):
+    res = 0
+
+    for output_obj in output['items']:
+        for eval_obj in eval_output['items']:
+
+            if output_obj['article_id'] == eval_obj['article_id']:
+
+                # Loop over parties
+                precision_parties_score = precision_parties(output_obj['parties'], eval_obj['parties'])
+                print(precision_parties_score)
+
+                # Loop over people
+
+
+# How many in the output should actually be in there
+def precision_parties(output_item_parties, eval_item_parties):
+    eval_count = len(eval_item_parties)
+    eval_count_simple = 0
+    output_score = 0
+
+    for op in output_item_parties:
+        for ep in eval_item_parties:
+            if op['abbreviation'] == ep['abbreviation']:
+                output_score += 1
+
+    for ep in eval_item_parties:
+        if ep['system_id'] != "000":
+            eval_count_simple += 1
+
+    return {
+        'eval_count': eval_count,
+        'eval_count_simple': eval_count_simple,
+        'output_score': output_score
+    }
+
+
+# How many of the eval are actually in the output
+def recall_parties(output, eval):
+    return 0
