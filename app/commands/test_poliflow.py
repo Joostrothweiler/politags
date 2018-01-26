68a8812d1dc935b511469d83c581a4fa2f6a2b7c
Joost Rothweiler <joostrothweiler@gmail.com>
2018-01-26 17:00:58 +0100

    change 5000 to 20000

--- app/commands/test_poliflow.py
+++ app/commands/test_poliflow.py
@@ -17,7 +17,7 @@ class TestPoliflowCommand(Command):
 
 def test_poliflow():
     """ Test Named Entity Algorithms."""
-    n_documents = 5000
+    n_documents = 200000
     documents = fetch_latest_documents(n_documents)
 
     start = time.time()
