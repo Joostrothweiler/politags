1f869af0e82d48f1882de7b12ff4f881cd86d90a
Max van Zoest <maxvanzoest@me.com>
2018-01-30 14:38:20 +0100

    poliflw json

--- app/static/scripts/poliflw.js
+++ app/static/scripts/poliflw.js
@@ -2,32 +2,7 @@
 
 let article = document.getElementById("article_container");
 
-let articleObject =
-    {
-        "date": "2017-12-01T00:00:00",
-        "date_granularity": 12,
-        "description": "<div class=\"node-content clearfix\"> &#13;\n  &#13;\n\t <p>Afgelopen dinsdag besloot de gemeenteraad om eindelijk een fatsoenlijk tarief te gaan betalen voor de huishoudelijke zorg.<br/>\nAlle gemeenten moeten per 1 april 2018 aan thuiszorgorganisaties het in de cao geregelde loon vergoeden van de bij hen werkende thuiszorgmedewerkers.</p>\n<p>Dankzij talrijke acties in het hele land&#160; - ook in Amersfoort met steun vanuit de lokale SP-afdeling -&#160; is dit resultaat bereikt:&#160; eindelijk &#8220;loon naar werken!&#8220;.</p>\n<p>SP-raadslid Bets Beltman maakte tijdens de raadsvergadering van de gelegenheid gebruik om de thuiszorg-medewerkers in Amersfoort te feliciteren met hun succes. Dit werd haar door de andere partijen niet echt in dank afgenomen &#8211; het was toch &#8216;maar een hamerstuk &#8216; en&#160; zij wilden liever wat vroeger naar huis&#8230;.</p>\n<p>Ook de wethouder, Fleur Imming van de PvdA,&#160; hield zich stil.</p>\n         <div class=\"zie-ook\">&#13;\n      <strong>Zie ook:</strong>&#13;\n      <ul>&#13;\n                            <li><a href=\"/dossier/thuiszorg\">Dossier: Thuiszorg</a></li>&#13;\n              </ul>&#13;\n    </div>&#13;\n    </div>&#13;\n&#13;\n\t\t\n",
-        "enrichments": {},
-        "location": "Amersfoort",
-        "meta": {
-            "pfl_url": "https://api.poliflw.nl/v0/cda_archives_vaals/4e8f2c46403d40023c701c94455eb7c502c16593",
-            "collection": "SP",
-            "original_object_id": "http://amersfoort.sp.nl/nieuws/2017/12/succes-thuiszorg-acties-ook-in-amersfoort-verzilverd",
-            "original_object_urls": {
-                "html": "http://amersfoort.sp.nl/nieuws/2017/12/succes-thuiszorg-acties-ook-in-amersfoort-verzilverd"
-            },
-            "processing_finished": "2017-12-05T20:56:11.729632",
-            "processing_started": "2017-12-05T20:24:15.921745",
-            "rights": "Undefined",
-            "source_id": "sp_archives_amersfoort"
-        },
-        "parties": [
-            "SP"
-        ],
-        "source": "Partij nieuws",
-        "title": "SUCCES THUISZORG-ACTIES OOK IN AMERSFOORT VERZILVERD!",
-        "type": "Partij"
-    };
+let articleObject = {{ result |tojson|safe }};
 
 //On opening the website we call the API to receive the question
 $(getQuestion());
@@ -71,6 +46,7 @@ function getQuestion() {
 }
 
 
+
 /**
  * Posts a response to the politags API
  * @param: response: the response to a question
@@ -82,9 +58,8 @@ function postAnswer(answer, questionId) {
     $.ajax({
         type: "POST",
         contentType: "application/json",
-        url: "http://localhost:5555/api/questions/" + questionId,
+        url: "/_answer/" + questionId,
         data: JSON.stringify(answer),
-        dataType: 'json',
         success: function (response) {
             console.log(response)
             updateCounter()
@@ -96,6 +71,10 @@ function postAnswer(answer, questionId) {
     })
 }
 
+
+// everything after this line can be copied from politags.js to make it compatible
+
+
 /**
  * This piece of code checks for a click on the responseButton class and posts the response to politags
  */
@@ -105,10 +84,13 @@ $('body').on('click', '.responseButton', function () {
         let answer = {
             "answer_id": answerId
         }
+
         let questionId = $(this).attr("question_id");
+
         postAnswer(answer, questionId)
     }
 );
+
 
 /**
  * This function highlights an entity in a given html element and saves the question we want to ask for this entity
