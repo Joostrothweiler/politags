f07bf6f56d5eee0c504cb2cfd34359453f2f4b1e
Joost Rothweiler <joostrothweiler@gmail.com>
2018-02-25 14:55:09 +0100

    quick verify use questioning api instead

--- app/commands/quick_verify.py
+++ app/commands/quick_verify.py
@@ -5,9 +5,9 @@ from flask_script import Command
 
 from app import db
 
-from app.models.models import EntityLinking, Verification
+from app.models.models import EntityLinking, Verification, Article
 from app.modules.common.utils import translate_doc
-from app.modules.computation.questioning import process_entity_verification
+from app.modules.computation.questioning import process_entity_verification, generate_questions
 from app.modules.poliflow.fetch import fetch_single_document
 
 logger = logging.getLogger('quick_verify')
@@ -22,37 +22,45 @@ class QuickVerifyCommand(Command):
 
 
 def quick_verify():
-    for current_entity_linking in EntityLinking.query.filter(EntityLinking.linkable_type == 'Politician').filter(
-            EntityLinking.initial_certainty == EntityLinking.updated_certainty).all():
-
-        entity = current_entity_linking.entity
-        politician = current_entity_linking.linkable_object
-        article = entity.article
+    for article in Article.query.all():
         document = fetch_single_document(article.id)
         simple_doc = translate_doc(document)
+        simple_doc['cookie_id'] = 'TERMINAL'
+        question_api_response = generate_questions(simple_doc, simple_doc['cookie_id'])
 
-        print(simple_doc['text_description'] + '\n')
-        print('Entity : \t{} from {} - {}'.format(entity.text, simple_doc['location'], simple_doc['parties']))
-        print('Politician: \t{} ({}) {} of {} from {}'.format(politician.title, politician.first_name, politician.full_name, politician.party, politician.municipality))
+        if 'error' in question_api_response:
+            print(question_api_response['error'])
 
-        response = input("Is this correct? [y,n,?,stop]: ")
-        response_str = str(response)
+        if 'error' not in question_api_response and question_api_response['label'] == 'PER':
+            print(simple_doc['text_description'] + '\n')
 
-        apidoc = simple_doc
-        apidoc['cookie_id'] = 'TERMINAL'
+            entity_linking_id = question_api_response['question_linking_id']
+            entity_linking = EntityLinking.query.filter(EntityLinking.id == entity_linking_id).first()
 
-        if response_str == 'y':
-            print('You said yes.')
-            apidoc['response_id'] = politician.id
-            process_entity_verification(current_entity_linking.id, apidoc)
-        elif response_str == 'n':
-            print('You said no.')
-            apidoc['response_id'] = -1
-            process_entity_verification(current_entity_linking.id, apidoc)
-        elif response_str == 'stop':
-            print('You want to stop.')
-            return
-        else:
-            print('You probably meant that you do not know. Lets continue.')
-        print('\n\n')
+            entity = entity_linking.entity
+            politician = entity_linking.linkable_object
+
+
+            print('Entity : \t{} from {} - {}'.format(entity.text, simple_doc['location'], simple_doc['parties']))
+            print('Politician: \t{} ({}) {} of {} from {}'.format(politician.title, politician.first_name, politician.full_name, politician.party, politician.municipality))
+
+            response = input("Is this correct? [y,n,?,stop]: ")
+            response_str = str(response)
+
+            apidoc = simple_doc
+
+            if response_str == 'y':
+                print('You said yes.')
+                apidoc['response_id'] = politician.id
+                process_entity_verification(entity_linking_id, apidoc)
+            elif response_str == 'n':
+                print('You said no.')
+                apidoc['response_id'] = -1
+                process_entity_verification(entity_linking_id, apidoc)
+            elif response_str == 'stop':
+                print('You want to stop.')
+                return
+            else:
+                print('You probably meant that you do not know. Lets continue.')
+            print('\n\n')
     print('Thanks!')
