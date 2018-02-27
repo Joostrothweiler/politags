8a24e61ea9688d042ccbfea2a8b7daa71c43837d
Joost Rothweiler <joostrothweiler@gmail.com>
2018-02-27 13:45:32 +0100

    fix name_long to name

--- app/modules/computation/questioning.py
+++ app/modules/computation/questioning.py
@@ -233,7 +233,7 @@ def generate_topics_json(article):
 
         topicobject = {
             "id": topic.id,
-            "text": topic.name_long,
+            "text": topic.name,
             "selected": selected
         }
 
