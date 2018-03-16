53b407b05f49c8c8f37344157522287e33b19552
Max van Zoest <maxvanzoest@me.com>
2018-03-16 11:19:49 +0100

    add path

--- app/static/scripts/politags_article.js
+++ app/static/scripts/politags_article.js
@@ -406,7 +406,7 @@ function setCookie(cookieName, cookieValue, expiryDays) {
     var date = new Date();
     date.setTime(date.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + date.toUTCString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + expires
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/"
 }
 
 
